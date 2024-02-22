f2bb52dcfde12eb910c01b2ae79e753d83751652
linus <lagbleze@st.ug.edu.gh>
2024-02-22 20:00:26 +0100

    Create siamese.py

---
+++ siamese.py
@@ -0,0 +1,26 @@
+
+
+#%%
+from torch_snippets import *
+
+#%%
+!wget https://www.dropbox.com/s/ua1rr8btkmpqjxh/face-detection.zip
+#!unzip face-detection.zip
+
+#%%
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
